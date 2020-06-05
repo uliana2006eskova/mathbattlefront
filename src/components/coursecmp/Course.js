e5ef3cc296f4f3ffe7121107690b75bbedc63d4f
Ulyana Eskova <ulyanaeskova@iMac.local>
2020-06-05 17:44:09 +0300

    get data from /login

--- src/components/coursecmp/Course.js
+++ src/components/coursecmp/Course.js
@@ -3,6 +3,10 @@ import './Course.css';
 import LeftMenu from './left-menu/left-menu.js';
 
 class Course extends React.Component {
+  componentDidAmount() {
+    const { token } = this.props.location.state.token;
+    console.log({token});
+  }
   render() {
     return (
       <div className="course">
